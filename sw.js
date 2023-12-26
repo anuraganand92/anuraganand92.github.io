6f9d95ad15d872350b8b3fc75fd48ccfbe7b63c3
anuraganand92 <anuragsr26@gmail.com>
2023-12-27 03:46:05 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ba3983cf024b200f4587.js"
+    "url": "webpack-runtime-469bf799232eb4dbf28b.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b861bc11751d1e4629eaa086a58690c9"
+    "revision": "d432aebf534ea98e9065683e86ffdd84"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
